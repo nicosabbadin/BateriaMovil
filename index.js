c440c23ac03e29d91a98826a7998c598f01e02cd
Nicolas Sabbadin <105654885+nicosabbadin@users.noreply.github.com>
2022-12-07 10:21:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+
+//va a detectar qeu boton hay quer tocar
+
+
+for (var i=0; i<document.querySelectorAll(".drum").length; i++ ){
+
+  document.querySelectorAll(".drum")[i].addEventListener('click', function () {
+
+var buttonInnerHTML= this.innerHTML;
+
+makeSound(buttonInnerHTML);
+
+buttonAnimation(buttonInnerHTML);
+
+
+});
+
+}
+
+// va a detectar que boton estas tocando y va a mandar al sonido
+
+document.addEventListener("keypress", function(event) {
+
+makeSound(event.key);
+
+buttonAnimation(event.key);
+
+});
+
+
+
+//darle un sonido a cada boton que va a tocar
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+       var tom1 = new Audio('sounds/tom-1.mp3');
+       tom1.play();
+      break;
+
+    case "a":
+       var tom2 = new Audio('sounds/tom-2.mp3');
+       tom2.play();
+      break;
+
+      case "s":
+         var tom3 = new Audio('sounds/tom-3.mp3');
+         tom3.play();
+        break;
+
+        case "d":
+           var tom4 = new Audio('sounds/tom-4.mp3');
+           tom4.play();
+          break;
+
+          case "j":
+             var snare = new Audio('sounds/snare.mp3');
+             snare.play();
+            break;
+
+            case "k":
+               var crash = new Audio('sounds/crash.mp3');
+               crash.play();
+              break;
+
+              case "l":
+                 var kick = new Audio('sounds/kick-bass.mp3');
+                 kick.play();
+                break;
+
+      default:
+
+  }
+}
+
+
+//agregar animacion al toque de tecla
+function buttonAnimation (currentKey){
+
+  var activeButton = document.querySelector("."+currentKey);
+
+  activeButton.classList.add('pressed');
+
+
+  setTimeout(function(){
+    activeButton.classList.remove('pressed');
+  },200);
+
+}
+
+
+
+//}
+//var audio = new Audio('sounds/tom-1.mp3');
+
+//  audio.play();
